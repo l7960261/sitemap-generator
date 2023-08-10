25ebf995c3418e39fcf04d393b7af595d10604b7
Wilson.Weng <l7960261@gmail.com>
2023-08-11 00:35:22 +0800

    add pptr.js

---
+++ src/pptr.js
@@ -0,0 +1,27 @@
+const puppeteer = require("puppeteer");
+
+const WebService = {
+  async run(url) {
+    try {
+      const browser = await puppeteer.launch({ headless: "new" });
+      const page = await browser.newPage();
+      await page.setViewport({ width: 1920, height: 926 });
+      await page.goto(url, { waitUntil: "networkidle0" });
+      const links = await page.$$eval("a", (collection) =>
+        collection.map((ele) => ele.href)
+      );
+      const imgs = await page.$$eval("img", (collection) =>
+        collection.map((ele) => ele.src)
+      );
+
+      console.log(links);
+      console.log(imgs);
+      await browser.close();
+      return { links, imgs };
+    } catch (error) {
+      return { links: [], imgs: [] }
+    }
+  },
+};
+
+module.exports = WebService;
