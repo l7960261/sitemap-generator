91c41327a4bed49091a730b2e77a5271af48d259
Wilson.Weng <l7960261@gmail.com>
2023-08-11 10:13:28 +0800

    Use function to fix counter scope

--- src/index.js
+++ src/index.js
@@ -4,9 +4,8 @@ const Log = require("./log");
 const Rules = require("./rules");
 const R = require("ramda");
 
-const { base,crawlLevel } = config;
-const baseUrlHashes = base.split('/').length;
-let counter = 0;
+const { base, crawlLevel } = config;
+const baseUrlHashes = base.split("/").length;
 const pending = [];
 const completed = [];
 const images = [];
@@ -17,17 +16,22 @@ const images = [];
 })();
 
 function autoCrawler() {
+  let counter = 0;
+
   while (pending.length > 0 && completed.length < 10) {
     const url = pending.pop();
     Log.log(`Queuing ${url}`);
 
     counter = counter + 1;
-
     completed.push(url);
-    WebService.run(url).then((result) => {
+    getData(url, counter);
+  }
+
+  function getData(url, index) {
+    return WebService.run(url).then((result) => {
       Log.log(`Data received for the ${url}`);
 
-      Log.log(`Data Queued ${counter} - Completed`);
+      Log.log(`Data Queued ${index} - Completed`);
       const { links, imgs } = result;
       images.push(imgs);
       const newLinks = Rules.format(links);
@@ -39,7 +43,9 @@ function autoCrawler() {
 function queue(urls) {
   const removingCompleted = R.uniq(R.without(completed, urls));
   const removingPending = R.without(pending, removingCompleted);
-  const removingIngoreLevels = removingPending.filter(url => url.split('/').length <= (baseUrlHashes+crawlLevel))
+  const removingIngoreLevels = removingPending.filter(
+    (url) => url.split("/").length <= baseUrlHashes + crawlLevel
+  );
 
   pending.push(...removingIngoreLevels);
   autoCrawler();
