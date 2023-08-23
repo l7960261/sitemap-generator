9db4faa9ea2111385b7fc66930f57eed017aba2b
Wilson.Weng <l7960261@gmail.com>
2023-08-23 08:56:27 +0800

    addClosingTag

--- src/xml.js
+++ src/xml.js
@@ -1,26 +1,31 @@
-const dayjs = require('dayjs');
+const dayjs = require("dayjs");
 const fs = require("fs");
+
 const XMLService = {
   create(links, imgs) {
-    const now = dayjs().format('YYYY-MM-DD')
+    const now = dayjs().format("YYYY-MM-DD");
     let str = `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`;
+
     links.forEach((link) => {
       str =
         str +
         `
   <url>
     <loc>${link}</loc>
-    <lastmod>${now}</lastmod>
-  </url>`;
+    <lastmod>${now}</lastmod>`;
+
+      str = str + addClosingTag("  </url>");
     });
 
-    str =
-      str +
-      `
-</urlset>`;
+    str = str + addClosingTag("</urlset>");
 
     fs.writeFileSync("./sitemap.xml", str, "utf-8");
   },
 };
+
+function addClosingTag(tag) {
+  return `
+${tag}`;
+}
 
 module.exports = XMLService;
